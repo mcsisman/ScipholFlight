f2a11c4862051de9fc893353abf7adb9c0fde2b1
mcsisman <61357620+mcsisman@users.noreply.github.com>
2022-08-07 18:42:06 +0300

    ios datetimepicker fix

--- src/components/FilterDialog.tsx
+++ src/components/FilterDialog.tsx
@@ -88,7 +88,7 @@ const FilterDialog: React.FC<FilterDialogProps> = (
 
       {showFromDate && (
         <DateTimePicker
-          style={{alignSelf: 'center'}}
+          style={{alignSelf: 'center', flex: 1}}
           testID="dateTimePickerFrom"
           value={fromDate}
           mode={'date'}
@@ -100,7 +100,7 @@ const FilterDialog: React.FC<FilterDialogProps> = (
       )}
       {showToDate && (
         <DateTimePicker
-          style={{alignSelf: 'center'}}
+          style={{alignSelf: 'center', flex: 1}}
           testID="dateTimePickerTo"
           value={toDate}
           mode={'date'}
